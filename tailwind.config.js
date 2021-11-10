6dd67d6513ba4b2f0ddfb3863769c29225775046
Said Rosas <saidmift@outlook.es>
2021-11-10 12:29:50 -0600

    Add the Tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+// *NOTE:* You can reference any value from the default theme if needed
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'sm': '480px',
+      'md': '768px',
+      'lg': '976px'
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      cyan: '#9cdbff'
+    },
+    extend: {
+      screens: {
+        '3xl': '1300px'
+      }
+    },
+    fill: theme => theme('colors')
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
